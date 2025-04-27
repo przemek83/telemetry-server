33a17ed772e65ac6847011d01fec4c657158bf0a
przemek83 <4788832+przemek83@users.noreply.github.com>
2025-04-27 17:37:37 +0200

    Remove unused includes and test functions from main.cpp; streamline server initialization.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
 #include <httplib.h>
-#include <cassert>
-#include <chrono>
-#include <cstdlib>
-#include <future>
-#include <iostream>
-#include <nlohmann/json.hpp>
-#include <thread>
 
 #include "EventHandler.h"
 #include "GetHandler.h"
@@ -15,88 +8,6 @@
 
 namespace
 {
-httplib::Params createParams()
-{
-    httplib::Params params;
-    params.emplace("resultUnit", "seconds");  // miliseconds
-    params.emplace("startTimestamp", "1");
-    params.emplace("endTimestamp", "3");
-    return params;
-}
-
-std::string createPayload(int date, const std::vector<int>& values)
-{
-    nlohmann::json payload = nlohmann::json::object();
-    payload["date"] = date;
-    payload["values"] = values;
-    return payload.dump();
-}
-
-void testGetting(httplib::Client& client, const std::string& event,
-                 int expectedMean)
-{
-    std::cout << "Client: GET sending" << std::endl;
-    httplib::Params params{createParams()};
-    auto res = client.Get("/paths/" + event + "/meanLength", params, {});
-    std::cout << "Client: GET reply code=" << res->status
-              << " body: " << res->body << std::endl;
-    assert(res->status == httplib::StatusCode::OK_200);
-    auto jsonResponse = nlohmann::json::parse(res->body);
-    // std::string expectedBody
-    assert(jsonResponse["mean"] == expectedMean);
-}
-
-void testPosting(httplib::Client& client)
-{
-    std::string jsonType{"application/json"};
-    // std::cout << "Client: POST sending" << std::endl;
-    // nlohmann::json payload = createPayloadForPostCall();
-    // std::cout << "Client: body: " << payload.dump() << std::endl;
-    auto res =
-        client.Post("/paths/start", createPayload(1, {3, 45, 78, 3}), jsonType);
-    assert(res->status == httplib::StatusCode::OK_200);
-    std::cout << "Client: POST Reply code=" << res->status
-              << " body: " << res->body << std::endl;
-    testGetting(client, "start", 32);
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-    res = client.Post("/paths/start", createPayload(2, {42, 34, 12}), jsonType);
-    assert(res->status == httplib::StatusCode::OK_200);
-    std::cout << "Client: POST Reply code=" << res->status
-              << " body: " << res->body << std::endl;
-    testGetting(client, "start", 31);
-
-    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-    res = client.Post("/paths/start", createPayload(3, {13, 123, 54, 31, 9}),
-                      jsonType);
-    assert(res->status == httplib::StatusCode::OK_200);
-    std::cout << "Client: POST Reply code=" << res->status
-              << " body: " << res->body << std::endl;
-    testGetting(client, "start", 37);
-
-    res =
-        client.Post("/paths/end", createPayload(1, {10, 20, 10, 20}), jsonType);
-    assert(res->status == httplib::StatusCode::OK_200);
-    testGetting(client, "end", 15);
-}
-
-auto getTestingClient(const std::string& address, int port)
-{
-    return [port, address]()
-    {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        httplib::Client client(address, port);
-
-        testPosting(client);
-
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-        // testGetting(client);
-    };
-}
-
 using httplib::Request;
 using httplib::Response;
 
@@ -116,25 +27,27 @@ std::function<void(const Request& req, Response& res)> wrapHandler(
 
 int main()
 {
-    std::cout << "Starting server..." << std::endl;
-
     std::string address("0.0.0.0");
     const int port{8080};
 
-    httplib::Server svr;
-    Telemetry telemetry;
     SimpleLogger logger;
+    logger.info("Starting server on " + address + ":" + std::to_string(port));
+
+    httplib::Server server;
+    Telemetry telemetry;
 
     GetHandler getHandler(telemetry, logger);
-    svr.Get("/paths/:event/meanLength", wrapHandler(getHandler));
+    server.Get("/paths/:event/meanLength", wrapHandler(getHandler));
 
     PostHandler postHandler(telemetry, logger);
-    svr.Post("/paths/:event", wrapHandler(postHandler));
+    server.Post("/paths/:event", wrapHandler(postHandler));
 
-    auto server =
-        std::async([&svr, address, port]() { svr.listen(address, port); });
-
-    getTestingClient(address, port)();
+    if (!server.listen(address, port))
+    {
+        logger.error("Failed to start server on " + address + ":" +
+                     std::to_string(port));
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
